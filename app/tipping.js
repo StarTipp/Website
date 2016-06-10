ad350308b26921c27d2e6a203009689df46ab5a6
Portikus <ahuisf@gmail.com>
2016-06-10 14:19:44 +0200

    Added a tipping page

--- app/tipping.js
+++ app/tipping.js
@@ -2,9 +2,14 @@
     var app = angular.module('Tipping', []);
 
     app.controller('TippingController', function () {
+        this.better = {};
         this.bettingGroup = bettingGroup;
-        this.bettingGroup.tournaments.push(tournament);
-        this.bettingGroup.better.push(better);
+        this.tournament = tournament;
+
+        better.bettingGroups.push(bettingGroup);
+        better.gameBets.push(gameBet);
+        bettingGroup.tournaments.push(tournament);
+        bettingGroup.better.push(better);
         tournament.bettingGroups.push(bettingGroup);
         tournament.tournamentRounds.push(tournamentRound);
         tournament.gameBets.push(gameBet);
@@ -15,71 +20,79 @@
         game.gamer2 = gamers[1];
         game.tournamentRound = tournamentRound;
         pointPolicy.tournamentRounds.push(tournamentRound);
-        gameBet.bettingGroup=bettingGroup;
+        gameBet.bettingGroup = bettingGroup;
         gameBet.better = better;
         gameBet.game = game;
         gameBet.tournament = tournament;
+
+        this.setBetter = function (betterName) {
+            this.better = better;
+            this.better.name = betterName;
+        }
+
+        this.saveTipps = function () {
+        }
 
     });
     //Models
 
 
     var tournament = {
-        name:"WCS",
-        bettingGroups:[],
-        tournamentRounds:[],
-        gameBets:[]
+        name: "WCS",
+        bettingGroups: [],
+        tournamentRounds: [],
+        gameBets: []
     }
 
     var bettingGroup = {
-        name:"Chaos-Krauts",
-        tournaments:[],
-        gameBets:[],
-        better:[]
+        name: "Chaos-Krauts",
+        tournaments: [],
+        gameBets: [],
+        better: []
     }
 
     var tournamentRound = {
-        tournament:{},
-        games:[],
-        pointPolicy:{},
-        playoffFormat:"BestOfOne"
+        tournament: {},
+        games: [],
+        pointPolicy: {},
+        playoffFormat: "BestOfOne"
     }
 
     var pointPolicy = {
-        tournamentRounds:[{}],
-        scorePoints:5
+        tournamentRounds: [{}],
+        scorePoints: 5
     }
 
     var gameBet = {
-        bettingGroup:{},
-        better:[],
-        tournament:{},
-        game:{},
-        predictedWinsGamer1:0,
-        predictedWinsGamer2:0
+        bettingGroup: {},
+        better: [],
+        tournament: {},
+        game: {},
+        predictedWinsGamer1: 0,
+        predictedWinsGamer2: 0
     }
 
     var game = {
-        gameBets:[],
-        tournamentRound:{},
-        gamer1:{},
-        gamer2:{},
-        winsGamer1:0,
-        winsGamer2:0,
-        gameState:"notStarted"
+        gameBets: [],
+        tournamentRound: {},
+        gamer1: {},
+        gamer2: {},
+        winsGamer1: 0,
+        winsGamer2: 0,
+        gameState: "notStarted"
     }
 
     var better = {
-        name:"Florian Ahuis",
-        bettingGroups:[],
-        gameBets:[]
+        name: "",
+        bettingGroups: [],
+        gameBets: []
     }
 
     var gamers = [{
-        name:"Scarlet",
-        games:[],
-    },{
-        name:"Nerchio",
-        games:[],
+        name: "Scarlet",
+        games: [],
+    }, {
+        name: "Nerchio",
+        games: [],
     }]
 })();
